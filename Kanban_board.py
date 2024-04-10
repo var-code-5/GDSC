553164a255a3fd0bdeadce1e490d169e859c79b0
var-code-5 <varshithisgod@gmail.com>
2024-04-11 02:18:27 +0530

    made section additions
    we can now add new sections to the board!!!

--- Kanban_board.py
+++ Kanban_board.py
@@ -3,8 +3,9 @@ import curses.textpad
 
 
 sections = []
+tasks = [[],[],[],[],[],]
 section_size = 30
-row_size = 4
+task_row_size = 6
 
 
 def draw_border(win):
@@ -20,35 +21,58 @@ class section:
         self.add_task_win = curses.newwin(3,section_size,curses.LINES-3,10+(self.sec_len)*section_size)
         self.add_task_win.addstr(1,0," + Add task")
         self.task_count = 0
+        self.add_section_win = curses.newwin(3,5,6,10+(self.sec_len)*section_size)
+        self.add_section_win.addstr(1,2,"+",curses.color_pair(1))
+        draw_border(self.add_section_win)
         draw_border(self.add_task_win)
         if(self.sec_len==0):
             self.heading_win.addstr(0,0,"TODO")
             self.section_win.refresh()
             self.heading_win.refresh()
             self.add_task_win.refresh()
+            self.add_section_win.mvwin(6,10+(self.sec_len+1)*section_size)
+            self.add_section_win.refresh()
         elif(self.sec_len==1):
             self.heading_win.addstr(0,0,"IN PROGRESS")
             self.section_win.refresh()
             self.heading_win.refresh()
             self.add_task_win.refresh()
+            self.add_section_win.mvwin(6,10+(self.sec_len+1)*section_size)
+            self.add_section_win.refresh()
         elif(self.sec_len==2):
             self.heading_win.addstr(0,0,"DONE")
             self.section_win.refresh()
             self.heading_win.refresh()
             self.add_task_win.refresh()
+            self.add_section_win.mvwin(6,10+(self.sec_len+1)*section_size)
+            self.add_section_win.refresh()
         else:
             self.heading = curses.textpad.Textbox(self.heading_win)
             self.section_win.refresh()
             self.heading.edit()
             self.heading_win.refresh()
             self.add_task_win.refresh()
+            self.add_section_win.mvwin(6,10+(self.sec_len+1)*section_size)
+            self.add_section_win.refresh()
 
 
 class task:
     def __init__(self,row:int,column) -> None:
-        self.task_win = curses.newwin(4,section_size-2,10+row*row_size,11+column*section_size)
+        # task window
+        self.task_win = curses.newwin(task_row_size,section_size-2,10+row*task_row_size,11+column*section_size)
         self.task_win.box()
         sections[column].task_count += 1
+        # adds color to the task
+        if(column == 0):
+            self.task_win.addstr(0,0,"Task :",curses.color_pair(3))
+        elif(column == 1):
+            self.task_win.addstr(0,0,"Task :",curses.color_pair(2))
+        elif(column == 2):
+            self.task_win.addstr(0,0,"Task :",curses.color_pair(1))
+        else:
+            self.task_win.addstr(0,0,"Task :")
+        #del_task
+        self.task_win.addstr(0,section_size-3,"x",curses.color_pair(3))
         self.task_win.refresh()
         pass
 
@@ -75,7 +99,6 @@ class kanban:
         sections.append(section())
         sections.append(section())
         window.refresh()
-        
 
 def initialize(window):
     window.clear()
@@ -84,14 +107,15 @@ def initialize(window):
 
 
 def add_task(section:int,max_lines)->None:
-    inital_point = 10
+    inital_point = 11
     row = sections[section].task_count
-    if(max_lines-3 > inital_point+row*row_size):
-        task_loc = task(row,section)
+    if(max_lines-3 > inital_point+row*task_row_size):
+        tasks[section].append(task(row,section))
     else:
-        pass 
+        curses.beep() 
 
 def mouse_event(x,y,max_lines,max_cols):
+    # adding the tasks
     if x>=10 and x<=40 and y>= max_lines-3 and y<= max_lines:
         add_task(0,max_lines)
     elif x>=40 and x<=70 and y>= max_lines-3 and y<= max_lines:
@@ -104,7 +128,20 @@ def mouse_event(x,y,max_lines,max_cols):
         add_task(3,max_lines)
 
 
+    if y>=6 and y<= 9 and x>=10+(len(sections))*section_size and x<=10+(len(sections))*section_size+4:
+        print("pressed")
+        if(10+(len(sections)+1)*section_size+4 < max_cols):
+            sections.append(section())
+        else:
+            curses.beep()
+    # removing the tasks
+
+
 def c_main(stdscr:'curses._CursesWindow'):
+    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_YELLOW ,curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_RED,curses.COLOR_BLACK)
+    curses.init_pair(4,curses.COLOR_WHITE,curses.COLOR_RED)
     curses.mousemask(-1)
     initialize(stdscr)
     boards = []
@@ -118,6 +155,7 @@ def c_main(stdscr:'curses._CursesWindow'):
             _,x,y,_,bitstate = curses.getmouse()
             if bitstate & curses.BUTTON1_CLICKED:
                 mouse_event(x,y,curses.LINES,curses.COLS)
+                print(x,y)
 
 def main()->int:
     return curses.wrapper(c_main)
